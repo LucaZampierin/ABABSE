7ceee44337c0b890d85528ab47b2854d7199c762
LucaZampierin <72515095+LucaZampierin@users.noreply.github.com>
2021-07-03 18:12:43 +0200

    Update dataReader2016.py

--- dataReader2016.py
+++ dataReader2016.py
@@ -1,6 +1,6 @@
 # Methods read the XML files
 #
-# https://github.com/LucaZampierin/ABSE
+# https://github.com/LucaZampierin/ABABSE
 #
 # Adapted from Trusca, Wassenberg, Frasincar and Dekker (2020). Changes have been made to adapt the methods
 # to the current project and to adapt the scripts to TensorFlow 2.5.
