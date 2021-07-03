58a2d63771230cd88f44eee41a2eb65e804e9ae4
LucaZampierin <72515095+LucaZampierin@users.noreply.github.com>
2021-07-03 18:12:58 +0200

    Update lcrModelU.py

--- lcrModelU.py
+++ lcrModelU.py
@@ -3,7 +3,7 @@
 
 # Unsupervised Left-Center-Right separated neural network with Rotatory attention (Uns-LCR-Rot)
 #
-# https://github.com/LucaZampierin/ABSE
+# https://github.com/LucaZampierin/ABABSE
 #
 # Adapted from Trusca, Wassenberg, Frasincar and Dekker (2020). Changes have been made to adapt the methods
 # to the current project and to adapt the scripts to TensorFlow 2.5.
